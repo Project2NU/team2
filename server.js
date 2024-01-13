8dd71e9eae9c5ab30a3d970a793cac189929d77c
1122c <118850768+1122c@users.noreply.github.com>
2024-01-13 12:48:51 -0800

    testing new branch

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const path = require("path");
-console.log("test");
+console.log("potato");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
