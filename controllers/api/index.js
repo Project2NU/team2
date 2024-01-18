b362341eff57f3f1ce9ac900afc1355dc5a0e08a
1122c <118850768+1122c@users.noreply.github.com>
2024-01-17 17:02:11 -0800

    edited api/index.js file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const bookRoutes = require("./bookRoutes");
 router.use("/users", userRoutes);
-router.use("/book", bookRoutes);
+router.use("/books", bookRoutes);
 
 module.exports = router;
