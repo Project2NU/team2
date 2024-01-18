dd5ef485cef18b200ead0d90f0562a9e5bc025e9
1122c <118850768+1122c@users.noreply.github.com>
2024-01-18 13:10:35 -0800

    edited more endpoint info, added book.js file in public folder

--- controllers/api/bookRoutes.js
+++ controllers/api/bookRoutes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const { User, Book } = require("../../models");
 
+// /api/books/
+
 // router.get("/test", (req, res) => {
 //   res.send("hello");
 // });
@@ -20,17 +22,26 @@ router.get("/", async (req, res) => {
 });
 //fun
 
+// /api/books
 router.post("/", async (req, res) => {
   //   console.log(req.body);
   const newBook = await Book.create(req.body);
   res.send(`you just created a book ${JSON.stringify(newBook)}`);
 });
 
-router.put("/:bookId", (req, res) => {
+// find book by bookId and update req.body.title and req.body.author
+router.put("/:bookId", async (req, res) => {
+  const newBook = await Book.update(req.body, {
+    where: { id: req.params.bookId },
+  });
+
   res.send(`you edit a book ${JSON.stringify(req.body)}`);
 });
 
-router.delete("/:bookId", (req, res) => {
+router.delete("/:bookId", async (req, res) => {
+  const newBook = await Book.destroy({
+    where: { id: req.params.bookId },
+  });
   res.send(`you deleted a book ${req.params.bookId}`);
 });
 
