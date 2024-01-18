4347bbc92b1a381e8847068a1d1a8ed8e1f458ae
1122c <118850768+1122c@users.noreply.github.com>
2024-01-17 17:49:28 -0800

    added and tested end points successfully

--- controllers/api/bookRoutes.js
+++ controllers/api/bookRoutes.js
@@ -1,21 +1,28 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, Book } = require("../../models");
 
 // router.get("/test", (req, res) => {
 //   res.send("hello");
 // });
 
-router.get("/:bookId", (req, res) => {
-  res.send(`you are getting book ${req.params.bookId}`);
+router.get("/:bookId", async (req, res) => {
+  let book = await Book.findOne({
+    where: { id: req.params.bookId },
+  });
+  //get book with this id
+  res.send(JSON.stringify(book));
 });
 
-router.get("/", (req, res) => {
-  res.send(`you are getting all books`);
+router.get("/", async (req, res) => {
+  let allBook = await Book.findAll();
+  //delete text from below
+  res.send(`you are getting all books ${JSON.stringify(allBook)}`);
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send(`you just created a book ${JSON.stringify(req.body)}`);
+router.post("/", async (req, res) => {
+  //   console.log(req.body);
+  const newBook = await Book.create(req.body);
+  res.send(`you just created a book ${JSON.stringify(newBook)}`);
 });
 
 router.put("/:bookId", (req, res) => {
