749fdc8522b64ceee6b222aac46777643ad4c7c3
Stuart Osborn <stuartgosborn22@gmail.com>
2024-01-18 18:57:27 -0600

    tidied up seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -4,8 +4,6 @@ const { User, Book } = require("../models");
 const userData = require("./userData.json");
 const bookSeedData = require("./bookSeedData.json");
 
-const userData = require("./userData.json");
-const bookData = require("./bookData.json");
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
