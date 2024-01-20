26295367343388c9d00468d76236decbee475470
1122c <118850768+1122c@users.noreply.github.com>
2024-01-20 10:29:22 -0800

    reviewRoutes.js testing only. this end point is not working, please do not use yet

--- models/Review.js
+++ models/Review.js
@@ -1,44 +1,44 @@
-const {Model, DataTypes} = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-class Review extends Model {};
+class Review extends Model {}
 
 Review.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      book_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'book',
-            key: 'id', 
-        },
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'user',
-            key: 'id', 
-        },
-      },
-      description: {
-        type: DataTypes.TEXT,
-        allowNull: false,
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    book_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "book",
+        key: "id",
       },
     },
-    {
-      sequelize,
-      timestamps: true,
-      freezeTableName: true,
-      underscored: true,
-      modelName: "review",
-    }
-  );
-  
-  module.exports = Review;
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "review",
+  }
+);
+
+module.exports = Review;
