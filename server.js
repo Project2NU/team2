2db909047ebd95bbdddf7b4217adb9bbe9afe695
Patric Xiong <pathlub@gmail.com>
2024-01-21 20:58:10 -0500

    Update

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
+// const newUrl = "https://example.com/new-page.html";
+// window.location.replace(newUrl);
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
